d1b6407acc6b1866fe152fad8cd0a0b0b049a4db
Daniel Standage <daniel.standage@nbacc.dhs.gov>
2018-12-14 15:23:50 +0000

    It liiiiiiiives!

---
+++ README.md
@@ -0,0 +1,11 @@
+# Perly *k*-mers
+
+Perl script for printing *k*-mer sequences.
+DNA sequences for a few mythical creatures are available in the `sequence/` directory for testing.
+
+```
+./kmers.pl < sequence/Basiliscus_vulgaris.fasta > Bvul-kmers.txt
+```
+
+In case it's not clear, this is not used for any serious work.
+Daniel Standage, 2018-12-14
---
+++ kmers.pl
@@ -0,0 +1,26 @@
+#!/opt/local/bin/perl5.8
+# ------------------------------------------------------------------------------
+# This is where I'd put my copyright notice...IF I HAD ONE!
+# Daniel Standage 2017
+# ------------------------------------------------------------------------------
+
+$k = 25;
+$/ = ">";
+
+$seqcount = 0;
+$kmercount = 0;
+while ($line = <STDIN>) {
+    chomp($line);
+    ($header, @seq) = split(/\n/, $line);
+    $sequence = join("", @seq);
+    if ($sequence eq "") {
+        next;
+    }
+    for $i (0 .. length($sequence) - $k + 1) {
+        $kmer = substr($sequence, $i, $k);
+        print("$kmer\n");
+        $kmercount++;
+    }
+    $seqcount++;
+}
+print(STDERR "Processed $kmercount k-mers in $seqcount sequences\n");
